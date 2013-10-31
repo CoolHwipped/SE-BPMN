4b8ce0a92ba2e8f1dffff5397935663ec90b516b
Catherine Sheehan <cesheehan3@plymouth.edu>
2013-10-31 16:43:58 -0400

    Update create_post.php

--- components/templates/create_post.php
+++ components/templates/create_post.php
@@ -1,16 +1,1 @@
-<form>
-<--form action is where to send the data, not sure where it goes yet!-->
-<form action="???" method="POST">
-
-	<p>Job Title</p> <input type="text" name="title">
-	<p>Pay Rate</p> <input type="text" name="pay">
-	<p>Job Description</p> <textarea name="description" rows="10" cols="45"></textarea>
-	<p>Qualifications</p> <textarea name="qualifications" rows="10" cols="45"></textarea>
-	<p>Link to Application</p> <input type="text" name="title">
-	<p>Contact Info</p> <input type="text" name="title">
-
-
-	<input type="submit" value="Send"><input type="reset" value="Clear">
-
-
-</form>
+//
