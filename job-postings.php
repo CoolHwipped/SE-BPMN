1ee8c3f5363845dfc2fdc2c467eacb063aba19cc
Tyler Ferm <tfermm@gmail.com>
2013-11-04 19:49:37 -0500

    added hook for database initialization

--- job-postings.php
+++ job-postings.php
@@ -15,8 +15,12 @@ License URI: http://www.gnu.org/licenses/gpl-2.0.html
 global $job_postings;
 
 require_once __DIR__ . '/components/class-job-postings.php';
+require_once __DIR__ . '/initialize/create_table.php';
+
+register_activation_hook( __FILE__, 'create_table' );
 
 if ( ! isset( $job_postings ) && ! $job_postings )
 {
 	$job_postings = new job_postings();
 }//end if
+
