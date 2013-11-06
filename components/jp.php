28b77ab9effd73ff612375f52a68b32a5619bc91
mlambert6 <mlambert6@plymouth.edu>
2013-11-06 00:26:14 -0500

    Update jp.php

--- components/jp.php
+++ components/jp.php
@@ -43,4 +43,28 @@ class jp
 		$newArray = array($this->title, $this->category, $this->expiration_date, $this->department, $this->job_description,$this->job_title,$this->pay_rate,$this->email,$this->app_link);
 		echo $newArray[0]. " -> ".$newArray[1]. " -> ".$newArray[2]. " -> ".$newArray[3]. " -> ".$newArray[4]. " -> ".$newArray[5]. " -> ".$newArray[6]. " -> ".$newArray[7]. " -> ".$newArray[8] ;	
 	}
+	public function queryPosts($orderedBy = null,$limit = 0,$ASC = true){
+		//$orderedBy is the column to order a post by
+		//$limit is to put a restraint on the number of posts to show
+		//ASC is the posts are in ascending order if true descending if false
+		global $wpdb;
+		$query = 'SELECT title,category,department
+					FROM wp_job_postings_posts';	
+		if($orderedBy != null){
+			$query .= ' ORDER BY '.$orderedBy.' ASC ';
+			if($ASC){
+				$query .= ' ASC ';
+			}
+			else{
+				$query .= ' DESC ';
+			}
+		}
+		if($limit != false && $limit > 0)	$query .= ' LIMIT '.$limit;
+		$query .= ';';
+		//pulls results
+		$ret = $wpdb->get_results($query,OBJECT);
+		//if $ret fails return false
+		if(!$ret)	return false;
+		else	return $ret;
+	}
 }
