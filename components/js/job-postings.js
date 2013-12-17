602eca4cd3c0dc2e51f43eb19a3eb9fc14531ab9
James Prehemo <jdprehemo@turing.cs.plymouth.edu>
2013-12-17 17:25:36 -0500

    Added field validation for the create post page. (phone number, email, pay rate)

--- components/js/job-postings.js
+++ components/js/job-postings.js
@@ -18,3 +18,37 @@ jQuery(document).ready( function( $ ){
 	});
 	jQuery( "#job-postings-datepicker-jquery" ).datepicker( "setDate", date );
 });
+
+jQuery(document).on("click", "#publish", function(){
+  
+  var errors = "";
+  var phone = jQuery("#job-postings-contact-phone").val();
+  var email = jQuery("#job-postings-contact-email").val();
+  var payrate = jQuery("#job-postings-payrate").val();
+  var phoneReg = /^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$/;
+  var emailReg = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+  var payReg = /^\d+(\.\d{1,2})?$/;
+  
+  //validation
+  email_is_valid = emailReg.test(email);
+  phone_is_valid = phoneReg.test(phone);
+  pay_is_valid = payReg.test(payrate);
+  
+  if(!phone_is_valid)
+  {
+    errors += "Phone Number is not in the correct format.\n"; 
+  }
+  if(!email_is_valid)
+  {
+    errors += "Email is in the incorrect format.\n";
+  }
+  if(!pay_is_valid)
+  {
+    errors += "Pay Rate must be decimal number. e.g 7.00 \n";
+  }
+  if(errors != "")
+  {
+     alert(errors);      
+     return false;
+  } 	
+});
